bb9b7bff7e251f1b4bf92b05221b6b4343811127
KentLin <snowcat_829@hotmail.com>
2021-11-08 21:38:30 +0800

    1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component, useState } from 'react'
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+import {Button, Modal} from 'react-bootstrap'
+import { forwardRef } from 'react';
+import MaterialTable from 'material-table'
+import NavBarHeader from './NavBarHeader.jsx'
+import Table1 from './Table1.jsx'
+import Modal1 from './Modal1.jsx'
+import StackChart from './page/StackChart.jsx'
+import VerticalBar from './page/VerticalBar.jsx'
+export default class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavBarHeader></NavBarHeader>
+                <VerticalBar></VerticalBar>
+                {/* <Modal1></Modal1> */}
+                {/* <Table1></Table1> */}
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/page/VerticalBar.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [4, 6, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+
+    {
+      label: '# of Votes',
+      data: [4, 6, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const options = {
+  responsive: true, maintainAspectRatio: true,
+
+  // width: 200,
+  // height: 200,
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+const VerticalBar = () => (
+  <>
+    <div className='header'>
+      <h1 className='title'>Vertical Bar Chart</h1>
+      <div className='links'>
+        <a
+          className='btn btn-gh'
+          href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/VerticalBar.js'
+        >
+          Github Source
+        </a>
+      </div>
+    </div>
+    <div>
+      {/* <Line data={this.state.chartData} options={this.state.chartOptions} width={this.state.width} height={this.state.height} /> */}
+      <Bar data={data} options={options} width={350} height={50} />
+    </div>
+
+  </>
+);
+
+export default VerticalBar;
